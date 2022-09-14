d82e493aa44d22775798107bf6674ea13a80296d
Divgun07 <71441340+Divgun07@users.noreply.github.com>
2022-09-14 17:50:39 +0530

    Add files via upload

---
+++ dataPreprocessing.py
@@ -0,0 +1,26 @@
+import csv
+dataset_1=[]
+dataset_2=[]
+with open("final.csv",'r')as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset_1.append(row)
+with open("archive_dataset_sorted1.csv",'r')as f:
+    csvreader=csv.reader(f)
+    for row in csvreader:
+        dataset_2.append(row)
+
+headers_1=dataset_1[0]
+planet_dataset_1=dataset_1[1:]
+headers_2=dataset_2[0]
+planet_dataset_2=dataset_2[1:]
+
+headers=headers_1+headers_2
+planet_data=[]
+for index,data_row in enumerate(planet_dataset_1):
+    planet_data.append(planet_dataset_1[index]+planet_dataset_2[index])
+
+with open("merged_datset.csv","a+") as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planet_data)
